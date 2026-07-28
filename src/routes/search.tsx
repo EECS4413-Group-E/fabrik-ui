d7b740b70a5baf9b9fbb22403afa3bb316bb90d8
kevinvasha <kejvinvasha04@gmail.com>
2026-07-27 20:50:25 -0400

    Fix type errors

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -1,19 +1,26 @@
-import { createFileRoute } from '@tanstack/react-router'
-import SearchPage from './-components/SearchPage'
+import { createFileRoute, type SearchSchemaInput } from '@tanstack/react-router';
+import SearchPage from './-components/SearchPage';
+
+type ProductsSearchInput = {
+  keyword?: string;
+  pageNumber?: number;
+  pageSize?: number;
+} & SearchSchemaInput;
 
 type ProductsSearch = {
-  keyword: string
-  pageNumber: number
-  pageSize: number
-}
+  keyword: string;
+  pageNumber: number;
+  pageSize: number;
+};
+
 export const Route = createFileRoute('/search')({
   component: SearchPage,
-  
-  validateSearch: (search: Record<string, unknown>): ProductsSearch => {
+
+  validateSearch: (search: ProductsSearchInput): ProductsSearch => {
     return {
       keyword: search.keyword || '',
       pageNumber: search.pageNumber || 0,
       pageSize: search.pageSize || 10,
     } as ProductsSearch;
-  }
-})
+  },
+});
